11177acbb9de8c3837a54a75e7206fc75e3f5cd5
vn54990 <avinash.ponugupati@walmart.com>
2018-02-07 12:39:36 +0530

    Data binding updated with unique datums concept

--- src/app/binding-data/binding-data.component.ts
+++ src/app/binding-data/binding-data.component.ts
@@ -82,7 +82,7 @@ export class BindingDataComponent implements OnInit {
       return "The index is " + i + " and the data is " + d;
    });
   }
-  
+
   dataStructure(){
     const x = [1, 2, 3]; // x coordinates
     const y = [1, 3, 2]; // y coordinates
@@ -106,7 +106,7 @@ export class BindingDataComponent implements OnInit {
         c: 'blue'
     }];
 
-    let mySvg = d3.select('#my-svg');
+    const mySvg = d3.select('#my-svg');
     this.circles = mySvg
       .selectAll('circle')
       .data(this.circlesData);
@@ -134,17 +134,17 @@ export class BindingDataComponent implements OnInit {
 
   dataChanges(){
     // change coordinates of the first circle:
-    this.circlesData[0].x = "10%";
-    this.circlesData[0].y = "10%";
+    this.circlesData[0].x = '10%';
+    this.circlesData[0].y = '10%';
 
     // add a new circle:
     this.circlesData.push({
-        x: "85%",
-        y: "85%",
+        x: '85%',
+        y: '85%',
         r: 15,
         c: 'yellow'
     });
-    let mySvg = d3.select('#my-svg');
+    const mySvg = d3.select('#my-svg');
     this.circles = mySvg
     .selectAll('circle')
     .data(this.circlesData);
@@ -177,12 +177,224 @@ export class BindingDataComponent implements OnInit {
   }
 
   removeCircles(){
-    let mySvg = d3.select('#my-svg');
+    const mySvg = d3.select('#my-svg');
     mySvg
     .selectAll('circle')
     .data([])
     .exit()
     .remove();
+  }
+
+  createMySvg(){
+    const data = [{
+        x: '25%',
+        y: '25%',
+        r: '5%',
+        c: 'red'
+    }, {
+        x: '50%',
+        y: '50%',
+        r: '5%',
+        c: 'green'
+    }, {
+        x: '75%',
+        y: '75%',
+        r: '5%',
+        c: 'blue'
+    }];
+
+  const svg = d3.select('#my-svg2');
+
+  const circles = svg
+      .selectAll('circle')
+      .data(data);
+
+  circles
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) {
+          return d.x;
+      })
+      .attr('cy', function(d) {
+          return d.y;
+      })
+      .attr('fill', function(d) {
+          return d.c;
+      })
+      .attr('r', function(d) {
+          return d.r;
+      });
+  }
+
+  UpdateDataWithIndex(){
+    // reset the `data` array with new circles:
+    const data = [{
+        x: '33%',
+        y: '33%',
+        r: '5%',
+        c: 'orange'
+    }, {
+        x: '66%',
+        y: '66%',
+        r: '5%',
+        c: 'cyan'
+    }, {
+        x: '25%',
+        y: '25%',
+        r: '5%',
+        c: 'magenta'
+    }];
+    const svg = d3.select('#my-svg2');
+    const circles = svg
+        .selectAll('circle')
+        .data(data);
+
+    // update (x,y) coordinates of existing elements:
+    circles
+        .transition()
+        .attr('cx', function(d) {
+            return d.x;
+        })
+        .attr('cy', function(d) {
+            return d.y;
+        });
+
+    // create new elements:
+    circles
+        .enter()
+        .append('circle')
+        .attr('cx', function(d) {
+            return d.x;
+        })
+        .attr('cy', function(d) {
+            return d.y;
+        })
+        .attr('fill', function(d) {
+            return d.c;
+        })
+        .attr('r', 0)
+        .transition()
+        .attr('r', function(d) {
+            return d.r;
+        });
+
+    // remove old elements:
+    circles
+        .exit()
+        .transition()
+        .attr('r', 0)
+        .remove();
+  }
+
+  createSvgWithID(){
+    const data = [{
+        id: 'before1',
+        x: '25%',
+        y: '25%',
+        r: '5%',
+        c: 'red'
+    }, {
+        id: 'before2',
+        x: '50%',
+        y: '50%',
+        r: '5%',
+        c: 'green'
+    }, {
+        id: 'before3',
+        x: '75%',
+        y: '75%',
+        r: '5%',
+        c: 'blue'
+    }];
+
+    const mySvg = d3.select('#my-svg3');
+
+    const circles = mySvg
+        .selectAll('circle')
+        .data(data, function(d) {
+            return d.id;
+        });
+
+    circles
+        .enter()
+        .append('circle')
+        .attr('cx', function(d) {
+            return d.x;
+        })
+        .attr('cy', function(d) {
+            return d.y;
+        })
+        .attr('fill', function(d) {
+            return d.c;
+        })
+        .attr('r', function(d) {
+            return d.r;
+        });
+  }
+
+  updateDataWithID(){
+    // reset the `data` array with new circles:
+    const data = [{
+        id: 'after1',
+        x: '33%',
+        y: '33%',
+        r: '5%',
+        c: 'orange'
+    }, {
+        id: 'after2',
+        x: '66%',
+        y: '66%',
+        r: '5%',
+        c: 'cyan'
+    }, {
+        id: 'after3',
+        x: '25%',
+        y: '25%',
+        r: '5%',
+        c: 'magenta'
+    }];
+    const mySvg = d3.select('#my-svg3');
+    const circles = mySvg
+        .selectAll('circle')
+        .data(data, function(d) {
+            return d.id;
+        });
+
+    // update (x,y) coordinates of existing elements:
+    circles
+        .transition()
+        .attr('cx', function(d) {
+            return d.x;
+        })
+        .attr('cy', function(d) {
+            return d.y;
+        });
+
+    // create new elements:
+    circles
+        .enter()
+        .append('circle')
+        .attr('cx', function(d) {
+            return d.x;
+        })
+        .attr('cy', function(d) {
+            return d.y;
+        })
+        .attr('fill', function(d) {
+            return d.c;
+        })
+        .attr('r', 0)
+        .transition()
+        .attr('r', function(d) {
+            return d.r;
+        });
+
+    // remove old elements:
+    circles
+        .exit()
+        .transition()
+        .attr('r', 0)
+        .remove();
   }
 
 }
